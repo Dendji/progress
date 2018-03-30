75cb77db198b7cfd48e5e705fc166fefb66517af
Denis <dendjet@gmail.com>
2018-03-30 11:57:18 +0300

    added event listeners

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
 import './sass/style.sass';
 
-console.info('Up and running');
+document.addEventListener('DOMContentLoaded', app);
+
+function app() {
+  let inputValue = document.getElementById('input-value');
+  let switchAnimate = document.getElementById('switch-animate');
+  let switchHide = document.getElementById('switch-hide');
+
+  inputValue.oninput = handleValue;
+
+  switchAnimate.onchange = function(event) {
+    console.info(`switch animate is ${this.checked}`);
+  };
+  switchHide.onchange = function(event) {
+    console.info(`switch hide is ${this.checked}`);
+  };
+}
+function handleValue(event) {
+  if (this.value.length > 2) {
+    this.value = this.value.slice(0, 2);
+  }
+}
