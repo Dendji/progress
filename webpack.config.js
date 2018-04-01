fd6d877f909f807fc757703e48e14f8700c4a534
Denis <dendjet@gmail.com>
2018-04-01 19:08:53 +0300

    docs folder added for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const config = {
     app: './app.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name].bundle.js'
   },
   devServer: {
